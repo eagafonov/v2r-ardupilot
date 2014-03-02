c54f3f6dd4c3e6af3da034222a596d78242be39d
Eugene Agafonov <e.a.agafonov@gmail.com>
2014-03-02 20:32:10 +0400

    [HAL_V2R] Implement RC input override
    RC Input override is used by MAVlink to set RC inputs from GCS

--- libraries/AP_HAL_V2R/RCInput.cpp
+++ libraries/AP_HAL_V2R/RCInput.cpp
@@ -2,37 +2,66 @@
 #include "RCInput.h"
 
 using namespace V2R;
+
 V2RRCInput::V2RRCInput()
-{}
+{
+    for(int i = 0; i < sizeof(_periods) / sizeof(_periods[0]); i++) {
+        _periods[i] = MID_CHANNEL_VALUE;
+    }
+
+    _periods[CHANNEL_THR] = MIN_CHANNEL_VALUE;
+}
 
 void V2RRCInput::init(void* machtnichts)
 {}
 
 uint8_t V2RRCInput::valid_channels() {
-    return 0;
+    return 0xFF; // All 8 channels are supported
 }
 
 uint16_t V2RRCInput::read(uint8_t ch) {
-    if (ch == 2) return 900; /* throttle should be low, for safety */
-    else return 1500;
+    if (ch > MAX_CHANNELS) {
+        return 0;
+    }
+
+    return _periods[ch];
 }
 
 uint8_t V2RRCInput::read(uint16_t* periods, uint8_t len) {
+    if (len > MAX_CHANNELS) {
+        len = MAX_CHANNELS;
+    }
     for (uint8_t i = 0; i < len; i++){
-        if (i == 2) periods[i] = 900;
-        else periods[i] = 1500;
+        periods[i] = _periods[i];
     }
     return len;
 }
 
 bool V2RRCInput::set_overrides(int16_t *overrides, uint8_t len) {
+    if (len > MAX_CHANNELS) {
+        len = MAX_CHANNELS;
+    }
+
+    for (int i = 0; i < len; i++) {
+        set_override(i, overrides[i]);
+    }
+
     return true;
 }
 
 bool V2RRCInput::set_override(uint8_t channel, int16_t override) {
-    return true;
+    if (channel < MAX_CHANNELS) {
+        if ((override >= MIN_CHANNEL_VALUE) && (override <= MAX_CHANNEL_VALUE)) {
+            _periods[channel] = override;
+        }
+        return true;
+    } else {
+        return false;
+    }
 }
 
 void V2RRCInput::clear_overrides()
-{}
+{
+    // Overrides is the only supporeted fro V2R yet
+}
 
--- libraries/AP_HAL_V2R/RCInput.h
+++ libraries/AP_HAL_V2R/RCInput.h
@@ -15,6 +15,16 @@ public:
     bool set_overrides(int16_t *overrides, uint8_t len);
     bool set_override(uint8_t channel, int16_t override);
     void clear_overrides();
+
+private:
+    static const int MAX_CHANNELS = 8;
+    static const uint16_t MIN_CHANNEL_VALUE = 1000;
+    static const uint16_t MAX_CHANNEL_VALUE = 2000;
+    static const uint16_t MID_CHANNEL_VALUE = (MIN_CHANNEL_VALUE + MAX_CHANNEL_VALUE) / 2;
+
+    static const uint8_t CHANNEL_THR = 3; // channel #3 is throttle
+
+    uint16_t _periods[MAX_CHANNELS];
 };
 
 #endif // __AP_HAL_V2R_RCINPUT_H__
